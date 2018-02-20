4a7779b3a133771115dc0628bc480acc7f1ccaf4
jacquetta <jacquettavb@hotmail.com>
2018-02-20 01:45:54 -0600

    checkpoint for audio playback completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -6,13 +6,42 @@ class Album extends Component {
     super(props);
 
     const album = albumData.find( album => {
-      console.log(album);
       return album.slug === this.props.match.params.slug
     });
 
     this.state = {
-      album: album
+      album: album,
+      currentSong: album.songs[0],
+      isPlaying: false
     };
+
+    this.audioElement = document.createElement('audio');
+    this.audioElement.src = album.songs[0].audioSrc;
+  }
+
+  play() {
+    this.audioElement.play();
+    this.setState({ isPlaying: true });
+  }
+
+  pause() {
+    this.audioElement.pause();
+    this.setState({ isPlaying: false });
+  }
+
+  setSong(song) {
+    this.audioElement.src = song.audioSrc;
+    this.setState({ currentSong: song });
+  }
+
+  handleSongClick(song) {
+    const isSameSong = this.state.currentSong === song;
+    if (this.state.isPlaying && isSameSong) {
+      this.pause();
+    } else{
+      if(!isSameSong) { this.setSong(song); }
+      this.play();
+    }
   }
 
   render() {
@@ -34,12 +63,17 @@ class Album extends Component {
           </colgroup>
           <tbody>
             {
-              this.state.album.songs.map( (album, index) =>
-              <tr>
-                <td className="ion-play"></td>
-                <td id="song-number">{index + 1}</td>
-                <td id="song-title">{this.state.album.songs[index].title}</td>
-                <td id="song-duration">{this.state.album.songs[index].duration}</td>
+              this.state.album.songs.map( (song, index) =>
+              <tr className="song" key={index} onClick={() => this.handleSongClick(song)}>
+                <td className="song-actions">
+                <button>
+                  <span className="song-number">{index + 1}</span>
+                  <span className="ion-play"></span>
+                  <span className="ion-pause"></span>
+                </button>
+                </td>
+                <td className="song-title">{song.title}</td>
+                <td className="song-duration">{song.duration}</td>
               </tr>
             )
           }
